96d7769c189c42719a9c5f81b82dbf24777ab1fc
sasi_p <sweetpea_th@hotmail.com>
2020-12-04 17:05:55 -0500

    Fixed the burger nav toggle issue.

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+// To open and close menu using burger nav
+
 function onClickMenu() {
 
     document.getElementById("menu").classList.toggle("change");
-
     document.getElementById("nav").classList.toggle("change");
+ }
+
+mybutton = document.getElementById("myBtn");
+
+
+// When user scrolls down 20px the button will show up
+ window.onscroll = function() {
+     scrollFunction();
+    };
+
+ function scrollFunction() {
+     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+         mybutton.style.display = "block";
+     } else {
+         mybutton.style.display = "none";
+     }
+ }
+
+// When user clicks on the button, scroll to the top
+ function topFunction() {
+     document.body.scrollTop = 0; // For Safari
+     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+ 
